6d96b1ef7ace49e7f3ef42711a8e6a00ba47185f
zfy-zhang <zfy_zang@163.com>
2018-11-05 08:37:00 +0800

    netty客户端登录

---
+++ src/main/java/com/vincent/protocol/response/LoginResponsePacket.java
@@ -0,0 +1,25 @@
+package com.vincent.protocol.response;
+
+import com.vincent.protocol.command.Packet;
+import lombok.Data;
+
+import static com.vincent.protocol.command.Command.LOGIN_RESPONSE;
+
+/**
+ * Created with IDEA
+ * author:vincent
+ * Date:2018/10/31
+ */
+@Data
+public class LoginResponsePacket extends Packet {
+    private boolean success;
+
+    private String reason;
+
+
+    @Override
+    public Byte getCommand() {
+        return LOGIN_RESPONSE;
+    }
+}
+
